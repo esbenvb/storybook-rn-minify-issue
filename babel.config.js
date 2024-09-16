81463f42fd86e5c8dcf315b3d045f456c444ab4f
Daniel Williams <dannyhyunsoowilliams@gmail.com>
2024-09-16 16:01:22 +0100

    fix: minify issue

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,4 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
-  plugins: [
-    // '@babel/plugin-transform-optional-chaining',
-    // '@babel/plugin-transform-nullish-coalescing-operator',
-    '@babel/plugin-transform-logical-assignment-operators',
-  ],
+  plugins: ['@babel/plugin-transform-logical-assignment-operators'],
 };
